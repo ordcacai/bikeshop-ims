d1b1e264127c5afb9af48b0261301a728b9446bf
jamesjanoras <jamesjanoras@gmail.com>
2023-05-13 05:58:32 +0800

    table fixing

--- admin/inventory.php
+++ admin/inventory.php
@@ -25,49 +25,80 @@
     $stmt1->store_result();
     $stmt1->fetch();
 
-    //3. products per page
-    $total_records_per_page = 5;
-    $offset = ($page_no-1) * $total_records_per_page;
-    $previous_page = $page_no - 1;
-    $next_page = $page_no + 1;
-    $adjacents = "2";
-    $total_no_of_pages = ceil($total_records/$total_records_per_page);
+    // 3. Return the total number of records for the selected category
+$category = isset($_GET['category']) ? $_GET['category'] : '';
+echo "Selected category: " . $category;
 
-    //4. get all products
-    $stmt2 = $conn->prepare("SELECT * FROM products LIMIT $offset, $total_records_per_page");
-    $stmt2->execute();
-    $products = $stmt2->get_result();//array
+if ($category != '') {
+    $stmt1 = $conn->prepare("SELECT COUNT(*) AS total_records FROM products WHERE product_category = ?");
+    $stmt1->bind_param("s", $category);
+} else {
+    $stmt1 = $conn->prepare("SELECT COUNT(*) AS total_records FROM products");
+}
+
+$stmt1->execute();
+$stmt1->bind_result($total_records);
+$stmt1->store_result();
+$stmt1->fetch();
+
+// Default values
+$total_records_per_page = 5;
+$offset = ($page_no - 1) * $total_records_per_page;
+$previous_page = $page_no - 1;
+$next_page = $page_no + 1;
+$adjacents = "2";
+$total_no_of_pages = ceil($total_records / $total_records_per_page);
+
+/// 4. Get all products
+$category = isset($_GET['category']) ? $_GET['category'] : '';
+if ($category != '') {
+    if ($category === 'parts_accessories') {
+        $stmt2 = $conn->prepare("SELECT * FROM products WHERE product_category = ? LIMIT ?, ?");
+        $stmt2->bind_param("sii", $category, $offset, $total_records_per_page);
+    } else {
+        $stmt2 = $conn->prepare("SELECT * FROM products WHERE product_category = ? LIMIT ?, ?");
+        $stmt2->bind_param("sii", $category, $offset, $total_records_per_page);
+    }
+} else {
+    $stmt2 = $conn->prepare("SELECT * FROM products LIMIT ?, ?");
+    $stmt2->bind_param("ii", $offset, $total_records_per_page);
+}
+$stmt2->execute();
+$products = $stmt2->get_result();
+
 
 ?>
 <div class="container-fluid">
     <div class="row" style="min-height:1000px">
-
         <?php include('sidemenu.php'); ?>
 
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <!-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 mt-2">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 mt-2">
                 <h1 class="mb-5">Dashboard</h1>
                 <div class="btn-toolbar mb-2 mb-md-0">
                     <div class="btn-group me-2"></div>
                 </div>
-            </div> -->
-            <h1 class="my-4">Inventory</h1>
-
+            </div>
+            <h1 class="mb-4">Inventory</h1>
 
             <!-- Centered Tab Menu -->
             <div class="d-flex justify-content-center mb-4">
                 <ul class="nav nav-tabs">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Ebikes</a>
+                        <a class="nav-link <?php echo (!isset($_GET['category']) || $_GET['category'] === 'Bike') ? 'active' : ''; ?>" href="?category=bike">Ebikes</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Bicycles</a>
+                        <a class="nav-link <?php echo ($_GET['category'] === 'bicycles') ? 'active' : ''; ?>" href="?category=bicycles">Bicycles</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Parts & Accessories</a>
+                        <a class="nav-link <?php echo (!isset($_GET['category']) || $_GET['category'] === 'parts_accessories') ? 'active' : ''; ?>" href="?category=parts_accessories">Parts & Accessories</a>
                     </li>
                 </ul>
             </div>
+
+            <?php if(isset($_GET['edit_success_message'])){ ?>
+                <p class="text-center" style="color: green;"><?php echo $_GET['edit_success_message']; ?></p>
+            <?php }?>
 
                 <?php if(isset($_GET['edit_success_message'])){ ?>
                     <p class="text-center" style="color: green;"><?php echo $_GET['edit_success_message']; ?></p>   
@@ -154,7 +185,7 @@
                             <?php } ?>
 
                         <li class="page-item">
-                            <a class="page-link <?= ($page_no >= $total_no_of_pages) ? 'disabled' : ''; ?> " <?= ($page_no < $total_no_of_pages) ? 'href=?page_no=' .$next_page : ''; ?>>Next</a>
+                        <a class="page-link <?= ($page_no >= $total_no_of_pages) ? 'disabled' : ''; ?> " <?= ($page_no < $total_no_of_pages) ? 'href=?page_no=' .$next_page : ''; ?>>Next</a>
                         </li>
 
                     </ul>
