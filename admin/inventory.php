32ab0fa863b0184b05ffffe7171d6a42a9455949
jamesjanoras <jamesjanoras@gmail.com>
2023-05-16 06:10:06 +0800

    product details added

--- admin/inventory.php
+++ admin/inventory.php
@@ -9,71 +9,74 @@
 
 <?php
 
-    // 1. Determine page number
-    $page_no = isset($_GET['page_no']) ? $_GET['page_no'] : 1;
+// Determine page number
+$page_no = isset($_GET['page_no']) ? $_GET['page_no'] : 1;
 
-    // 2. Return total number of products
-    $stmt1 = $conn->prepare("SELECT COUNT(*) AS total_records FROM products");
-    $stmt1->execute();
-    $stmt1->bind_result($total_records);
-    $stmt1->store_result();
-    $stmt1->fetch();
+// Return the total number of records for the selected category
+$category = isset($_GET['category']) ? $_GET['category'] : 'Ebike';
+$category_condition = '';
 
-    // 3. Return the total number of records for the selected category
-    $category = isset($_GET['category']) ? $_GET['category'] : '';
-    echo "Selected category: " . $category;
+if ($category === 'Ebike') {
+    $category_condition = "WHERE product_category = 'Ebike'";
+} else if ($category === 'bicycles') {
+    $category_condition = "WHERE product_category = 'Bike'";
+} else if ($category === 'parts_accessories') {
+    $category_condition = "WHERE product_category = 'parts'";
+}
 
-    if ($category != '') {
-        $stmt1 = $conn->prepare("SELECT COUNT(*) AS total_records FROM products WHERE product_category = ?");
-        $stmt1->bind_param("s", $category);
-    } else {
-        $stmt1 = $conn->prepare("SELECT COUNT(*) AS total_records FROM products");
-    }
+// Count the total number of records
+$stmt1 = $conn->prepare("SELECT COUNT(*) AS total_records FROM products $category_condition");
+$stmt1->execute();
+$stmt1->bind_result($total_records);
+$stmt1->store_result();
+$stmt1->fetch();
 
-    $stmt1->execute();
-    $stmt1->bind_result($total_records);
-    $stmt1->store_result();
-    $stmt1->fetch();
+// Default values
+$total_records_per_page = 5;
+$total_no_of_pages = ceil($total_records / $total_records_per_page);
 
-    // Default values
-    $total_records_per_page = 5;
-    $offset = ($page_no - 1) * $total_records_per_page;
-    $previous_page = $page_no - 1;
-    $next_page = $page_no + 1;
-    $adjacents = 2;
-    $total_no_of_pages = ceil($total_records / $total_records_per_page);
+// Validate and adjust page number if out of range
+$page_no = max(1, min($page_no, $total_no_of_pages));
 
-    /// 4. Get products based on category and pagination
-    $category = isset($_GET['category']) ? $_GET['category'] : '';
-    if ($category === 'bike') {
-        $stmt2 = $conn->prepare("SELECT * FROM products WHERE product_category = 'Bike' LIMIT ?, ?");
-    } else if ($category === 'bicycles') {
-        $stmt2 = $conn->prepare("SELECT * FROM products WHERE product_category = 'Bike' LIMIT ?, ?");
-    } else if ($category === 'parts_accessories') {
-        $stmt2 = $conn->prepare("SELECT * FROM products WHERE product_category = 'parts' LIMIT ?, ?");
-    } else {
-        $stmt2 = $conn->prepare("SELECT * FROM products WHERE product_category = 'bikes' LIMIT ?, ?");
-    }
-    $stmt2->bind_param("ii", $offset, $total_records_per_page);
-    $stmt2->execute();
-    $products = $stmt2->get_result();
+// Calculate the offset
+$offset = ($page_no - 1) * $total_records_per_page;
+
+// Get products based on category and pagination
+$category_condition = "";
+$params = array($offset, $total_records_per_page);
+
+if ($category === 'Ebike') {
+    $category_condition = "WHERE product_category = 'Ebike'";
+} else if ($category === 'bicycles') {
+    $category_condition = "WHERE product_category = 'Bike'";
+} else if ($category === 'parts_accessories') {
+    $category_condition = "WHERE product_category = 'parts'";
+}
+
+$query = "SELECT * FROM products $category_condition LIMIT ?, ?";
+$stmt2 = $conn->prepare($query);
+$stmt2->bind_param("ii", ...$params);
+$stmt2->execute();
+$products = $stmt2->get_result();
+
 ?>
 <?php include('sidemenu.php'); ?>
 
 <div class="main-content">
-    <div class="container-fluid">
+        <div class="container-fluid">
             <h1 class="my-4">Inventory</h1>
-            <!-- Centered Tab Menu -->
-<div class="d-flex justify-content-center mb-4">
+
+           <!--Tab Menu -->
+           <div class="d-flex justify-content-left mb-4">
     <ul class="nav nav-tabs">
         <li class="nav-item">
-            <a class="nav-link <?php echo ((!isset($_GET['category']) || $_GET['category'] === 'bike') ? 'active' : ''); ?>" href="?category=bike">Ebikes</a>
+            <a class="nav-link <?= ($category === 'Ebike' || $category === '') ? 'active' : ''; ?>" href="?category=Ebike&page_no=<?= $page_no ?>">Ebikes</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link <?php echo ((!isset($_GET['category']) || $_GET['category'] === 'bicycles') ? 'active' : ''); ?>" href="?category=bicycles">Bicycles</a>
+            <a class="nav-link <?= ($category === 'bicycles') ? 'active' : ''; ?>" href="?category=bicycles&page_no=<?= $page_no ?>">Bicycles</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link <?php echo ((!isset($_GET['category']) || $_GET['category'] === 'parts_accessories') ? 'active' : ''); ?>" href="?category=parts_accessories">Parts & Accessories</a>
+            <a class="nav-link <?= ($category === 'parts_accessories') ? 'active' : ''; ?>" href="?category=parts_accessories&page_no=<?= $page_no ?>">Parts & Accessories</a>
         </li>
     </ul>
 </div>
@@ -144,27 +147,32 @@
                     </tbody>
                 </table>
 
-                    <nav aria-label="Page navigation example" class="text-center">
+                   <!-- Add Product button -->
+    <div class="text-center mt-4">
+        <a href="add_product.php" class="btn btn-primary">Add Product</a>
+    </div>
+</div>
+
+                <nav aria-label="Page navigation example" class="text-center">
                     <ul class="pagination mt-5 justify-content-center">
                         <li class="page-item">
-                            <a class="page-link <?= ($page_no <= 1) ? 'disabled' : ''; ?> " <?= ($page_no > 1) ? 'href=?page_no=' .$previous_page : ''; ?>>Previous</a>
+                            <a class="page-link <?= ($page_no <= 1) ? 'disabled' : ''; ?> " <?= ($page_no > 1) ? 'href="?category=' . $category . '&page_no=' .$previous_page : ''; ?>>Previous</a>
                         </li>
 
+                        <li class="page-item"><a href="?category=<?= $category ?>&page_no=1" class="page-link">1</a></li>
+                        <li class="page-item"><a href="?category=<?= $category ?>&page_no=2" class="page-link">2</a></li>
 
-                        <li class="page-item"><a href="?page_no=1" class="page-link">1</a></li>
-                        <li class="page-item"><a href="?page_no=2" class="page-link">2</a></li>
-
-                            <?php if($page_no >= 3) { ?>
-                                <li class="page-item"><a href="#" class="page-link">...</a></li>
-                                <li class="page-item"><a href="?page_no=<?= $page_no; ?>" class="page-link"><?= $page_no; ?></a></li>
-                            <?php } ?>
+                        <?php if ($page_no >= 3) { ?>
+                            <li class="page-item"><a href="#" class="page-link">...</a></li>
+                            <li class="page-item"><a href="?category=<?= $category ?>&page_no=<?= $page_no; ?>" class="page-link"><?= $page_no; ?></a></li>
+                        <?php } ?>
 
                         <li class="page-item">
-                        <a class="page-link <?= ($page_no >= $total_no_of_pages) ? 'disabled' : ''; ?> " <?= ($page_no < $total_no_of_pages) ? 'href=?page_no=' .$next_page : ''; ?>>Next</a>
+                            <a class="page-link <?= ($page_no >= $total_no_of_pages) ? 'disabled' : ''; ?> " <?= ($page_no < $total_no_of_pages) ? 'href="?category=' . $category . '&page_no=' .$next_page : ''; ?>>Next</a>
                         </li>
-
                     </ul>
                 </nav>
+
 
                 <div class="p-10">
                     <strong>Page <?= $page_no; ?> of <?= $total_no_of_pages ?></strong>
