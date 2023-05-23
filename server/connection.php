2766c212d72a4534d13859e2e7f4521693153535
ordcacai <ordonezerica11@gmail.com>
2023-05-23 12:35:34 +0800

    test

--- server/connection.php
+++ server/connection.php
@@ -1,19 +1,8 @@
 <?php
-        $host = 'srv598.hstgr.io'; // Replace with your MySQL server hostname or IP address
-        $db = 'u564301412_inventory_db'; // Replace with your database name
-        $user = 'u564301412_inventory_db'; // Replace with your MySQL username
-        $password = 'Gw*8ZG2I'; // Replace with your MySQL password
 
-        // Create a connection
-        $conn = new mysqli($host, $user, $password, $db);
+$conn = mysqli_connect("localhost", "root","","inventory_db") 
+        or die("Couldn't connect to the database");
+        
 
-        // Check the connection
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
 
-        echo "Connected successfully";
-
-        // Close the connection
-        $conn->close();
 ?>
