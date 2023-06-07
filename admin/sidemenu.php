3a99e0307d88cc6f7225b3e3e99b919965730aaa
zedrick.devera <zedrickdevera@gmail.com>
2023-06-08 00:41:05 +0800

    order cancel request added

--- admin/sidemenu.php
+++ admin/sidemenu.php
@@ -38,6 +38,7 @@ if(!isset($_SESSION['logged_in'])){
           <li><a href="orders.php">View Orders</a></li>
           <li><a href="retail.php">Add Order</a></li>
           <li><a href="invoice.php">Order Invoice</a></li>
+          <li><a href="cancel_order.php">Cancel Request</a></li>
         </ul>
       </li>
       <li>
---
+++ cancel_order_email.php
@@ -0,0 +1,82 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+include('layouts/header.php');
+include('server/connection.php');
+
+if(!isset($_SESSION['logged_in'])){
+
+    header('location: login.php');
+    exit;
+
+}else if(isset($_POST['cancel_order_btn'])){
+
+    require ('vendor/autoload.php');
+    require ('admin/credential.php');
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+
+        $user_name = $row['user_name'];
+        $body = '';
+        $body .="<html>
+        <head>
+        <style type='text/css'> 
+        body {
+        font-family: Calibri;
+        font-size:16px;
+        color:#000;
+        }
+        </style>
+        </head>
+        <body>
+        <div class='center'>
+        <p>$message</p>
+        </div>
+        </body>
+        </html>";
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = EMAIL;                     //SMTP username
+        $mail->Password   = PASS;                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress(EMAIL, 'Vykes MNL');     //Add a recipient
+        $mail->addReplyTo($email, $name);
+
+        //Attachments
+        // $mail->addAttachment($file_location.$file_name);         //Add attachments
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        $mail->AltBody = $body;
+
+        $mail->send();
+        header('location: cancel_order.php?request_sent=Cancel request has been emailed successfully!');
+        exit; 
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+}else{
+    header('location: cancel_order.php?request_failed=Cancel request unsuccessful.');
+}
+?>
