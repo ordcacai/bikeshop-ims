370f30925c05b62f92cbb7981c020f5dc6ee1704
koihannn <yandrewab2000@gmail.com>
2023-06-08 18:28:31 +0800

    filter range

--- shop.php
+++ shop.php
@@ -109,7 +109,7 @@ if(isset($_POST['search'])){
                         <div class="col-lg-12 col-md-12 col-sm-12">
 
                             <p>Price</p>
-                            <input type="range" class="form-range w-50" name="price" value="<?php if(isset($price)){echo $price;}else{echo "100";} ?>" min="1"  max="1000"id="customRange2">
+                            <input type="range" class="form-range w-50" name="price" value="<?php if(isset($price)){echo $price;}else{echo "100";} ?>" min="1000"  max="50000"id="customRange2">
                             <div class="w-50">
                                 <span style="float: left;">₱1</span>
                                 <span style="float: right;">₱1000</span>
