a26adc014676b65dca807fa5b4d8717ac7921d7f
zedrick.devera <zedrickdevera@gmail.com>
2023-06-09 02:52:24 +0800

    try

--- shop.php
+++ shop.php
@@ -82,7 +82,7 @@ if(isset($_POST['search'])){
 
 <!-- Search -->
 <div id="shop-container">
-<section id="search" class="my-5 py-5"> 
+<section id="search" class="my-5 py-5 ms-5"> 
         <div class="container mt-5 py-5">
             <h4 class="text-center">Search Products</h4>
             <hr>
