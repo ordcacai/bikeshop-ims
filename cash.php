42e93c427584c163791396b36416f1ae376bf1cf
zedrick.devera <zedrickdevera@gmail.com>
2023-06-09 03:59:25 +0800

    t

--- cash.php
+++ cash.php
@@ -6,7 +6,7 @@
         <div  class="container">
             <h1 class="text-center mt-5 pt-4">Flexible Mode of Payments</h1>
             <p class="text-center">We accept online payments through Bank Transfer or Gcash.</p>
-            <img src="assets/imgs/mop_bg.jpg" alt="" width="600px" height="450px" style="display: block; margin-left: auto; margin-right: auto; width: 50%;">
+            <img src="assets/imgs/mop_bg.jpg" alt="" style="display: block; margin-left: auto; margin-right: auto; width: 50%;">
         </div>
 
 
