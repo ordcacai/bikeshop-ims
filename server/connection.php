168a732b8a377fa9a48de582b9249d337ea99dcc
ordcacai <ordonezerica11@gmail.com>
2023-06-08 17:32:04 +0800

    con test

--- server/connection.php
+++ server/connection.php
@@ -1,5 +1,5 @@
 <?php
-        $host = 'srv598.hstgr.io';
+        $host = '31.220.110.201';
         $db = 'u564301412_vmnl_db';
         $user = 'u564301412_vmnl_db';
         $password = 'rX^raBl57';
