2c5282d219af5c89fe3b87ea41b5defea4dcfc00
ordcacai <ordonezerica11@gmail.com>
2023-06-09 22:18:36 +0800

    connnn

--- server/connection.php
+++ server/connection.php
@@ -1,8 +1,8 @@
 <?php
-        $host = 'localhost'; 
+        $host = '31.220.110.201'; 
         $db = 'u564301412_vmnl_db'; 
         $user = 'u564301412_vmnl_db'; 
-        $password = 'VykesMNL23'; 
+        $password = 'VykesMNL2023'; 
 
         // Create a connection
         $conn = new mysqli($host, $user, $password, $db);
